3d4498186ec2466f32bf3a3f9fb3b03c3d3f4c5a
Aura Varela <ac9395@gmail.com>
2021-06-01 09:11:47 -0500

    cambios ejercicos practicos HTML CSS

---
+++ Sesion 1 JS/scripts/script.js
@@ -0,0 +1,21 @@
+/*Ejemplo de manejo de constantes que se debe declarar y
+asignar en la misma linea*/
+const nombre = "Pablo";
+console.log(nombre);
+
+/*Ejemplo de manejo de constantes que se puede declarar
+y asignar por separado*/
+let nombre1;
+nombre1 = "Sonia";
+console.log (nombre1);
+
+
+/*Constantes no se puede reasignar su valor*/
+const nombre = "Jenny";
+const apellido = "Montoya";
+
+apellido = "Gómez";
+
+console.log(nombre,apellido);
+
+
